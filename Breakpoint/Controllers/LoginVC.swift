43506bf92c6d2feba0298c3ecd500a67fae1b22c
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-25 10:01:00 -0500

    enabled user to log in by email. User is stored in firebase

--- Breakpoint/Controllers/LoginVC.swift
+++ Breakpoint/Controllers/LoginVC.swift
@@ -9,27 +9,48 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var emailField: InsetTextField!
+    @IBOutlet weak var passwordField: InsetTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        emailField.delegate = self
+        passwordField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func signInBtnPressed(_ sender: Any) {
+        if emailField.text != nil || passwordField != nil {
+            AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!, loginComplete: { (complete, loginError) in
+                if complete {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(String(describing: loginError?.localizedDescription))
+                }
+                
+                AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, userCreationComplete: { (complete, registrationError) in
+                    if complete {
+                        AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, loginComplete: { (complete, nil) in
+                            self.dismiss(animated: true, completion: nil)
+                            print("Successfully registered user")
+                        })
+                    } else {
+                        print(String(describing: registrationError?.localizedDescription))
+                    }
+                })
+            })
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension LoginVC: UITextFieldDelegate {
+    
 }
