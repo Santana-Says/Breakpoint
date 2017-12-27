15eddf987dcd7cd07ac8607e0aaa867210c5ecbb
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-26 22:28:42 -0500

    Created profile ui as a new tabbed icon

---
+++ Breakpoint/Controllers/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  Breakpoint
+//
+//  Created by Jeffrey Santana on 12/26/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: terminalStyleLabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+    }
+    
+
+}
