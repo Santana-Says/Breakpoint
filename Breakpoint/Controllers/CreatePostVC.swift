a3ac06331ea650a2ede965cbc997811b2e88b592
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-27 22:06:00 -0500

    updated the feed table to show the newest on top and all the username labels display the current users email

--- Breakpoint/Controllers/CreatePostVC.swift
+++ Breakpoint/Controllers/CreatePostVC.swift
@@ -24,6 +24,11 @@ class CreatePostVC: UIViewController {
         textView.delegate = self
         sendBtn.bindToKeyboard()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.emailLbl.text = Auth.auth().currentUser?.email
+    }
 
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
