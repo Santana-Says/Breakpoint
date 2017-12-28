5d54c41a8d33965d4c3ad976bc266df870895dec
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2017-12-27 20:53:57 -0500

    created a class to hold feed messages that come from the database, and displayed them on cells in the feed screen

---
+++ Breakpoint/Controllers/FeedVC.swift
@@ -0,0 +1,53 @@
+//
+//  FirstViewController.swift
+//  Breakpoint
+//
+//  Created by Jeffrey Santana on 12/24/17.
+//  Copyright © 2017 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //variables
+    var messageArray = [Message]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DataService.instance.getAllFeedMessages { (returnedMessagesArray) in
+            self.messageArray = returnedMessagesArray
+            self.tableView.reloadData()
+        }
+    }
+    
+
+}
+
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell") as? FeedCell else { return UITableViewCell() }
+        let image = UIImage(named: "defaultProfileImage")
+        let message = messageArray[indexPath.row]
+        
+        cell.configCell(profileImg: image!, email: message.senderId, content: message.content)
+        return cell
+    }
+}
