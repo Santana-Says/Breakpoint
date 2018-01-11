0d0e2cfd7c5489c2bc572b30582655dc495f17cf
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2018-01-10 23:16:43 -0500

    only groups the user is a part of appear in the groups section

--- Breakpoint/Controllers/GroupsVC.swift
+++ Breakpoint/Controllers/GroupsVC.swift
@@ -12,6 +12,9 @@ class GroupsVC: UIViewController {
     
     //Outlets
     @IBOutlet weak var groupsTableView: UITableView!
+    
+    //Variables
+    var groupsArray = [Group]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,17 @@ class GroupsVC: UIViewController {
         groupsTableView.dataSource = self
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //listens for any changes on firebase
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshot) in
+            DataService.instance.getAllGroups { (usersGroups) in
+                self.groupsArray = usersGroups
+                self.groupsTableView.reloadData()
+            }
+        }
+    }
 
 }
 
@@ -29,14 +43,15 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "GroupCell") as? GroupCell else {
             return UITableViewCell()
         }
-        cell.configCell(title: "Nerd Herd", description: "Nerd Movies", memberCount: 3)
+        let group = groupsArray[indexPath.row]
+        cell.configCell(title: group.groupTitle, description: group.groupDesc, memberCount: group.memberCount)
         
         return cell
     }
