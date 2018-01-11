c6d71f87c2eb558f282e6a4bca76f4fe8a070af5
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2018-01-10 22:36:57 -0500

    created group cells to hold the users groups

--- Breakpoint/Controllers/GroupsVC.swift
+++ Breakpoint/Controllers/GroupsVC.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class GroupsVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var groupsTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        groupsTableView.delegate = self
+        groupsTableView.dataSource = self
     }
 
 
 }
 
+extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "GroupCell") as? GroupCell else {
+            return UITableViewCell()
+        }
+        cell.configCell(title: "Nerd Herd", description: "Nerd Movies", memberCount: 3)
+        
+        return cell
+    }
+}
